a622948579a73b6a76955aae75b107cbf684d5ea
DmitryMazaev <118983899+DmitryMazaev@users.noreply.github.com>
2023-04-01 19:29:55 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,12 +5,12 @@ public class Main
     public static void main (String[] args) 
     {
         //Задача 1
-        //Z1 Z1 = new Z1();
-        //Z1.sortNumbers();
+        Z1 Z1 = new Z1();
+        Z1.sortNumbers();
         
         //Задача 2
-        //Z2 Z2 = new Z2();
-        //Z2.delList();
+        Z2 Z2 = new Z2();
+        Z2.delList();
 
         //Задача 3
         Z3 Z3 = new Z3();
